6a848272f575f68757f14e75a9397a9144b0e0b8
Soldout <Buikemivity@gmail.com>
2026-06-21 12:05:12 -0700

    practical_set

--- level1_practice/1_practice.py
+++ level1_practice/1_practice.py
@@ -1,1 +1,37 @@
+'''Q1: Your First Program
 
+Write a program that prints: 
+Hello, World! Welcome to Python.
+'''
+
+print("Hello, World! Welcome to Python. ") #solution make sure the capitalise words and comma are where they are meant to be
+
+
+'''Q2: Print a Poem
+
+Write a program that prints the following poem using a single statement: 
+Twinkle, twinkle, little star, 
+How I wonder what you are!
+
+(Hint: Use \n for a new line.)
+'''
+print("Twinkle, twinkle, little star, "  "\nHow I wonder what you are!") # method1
+
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!''') # method 2
+
+print("Twinkle, twinkle, little star, \nHow I wonder what you are!") #method 3
+
+'''Q3: Variables & Data Types
+
+Create variables to store: - Your name (string)
+-	Your age (integer)
+-	Your height in meters (float)
+-	A boolean value representing whether you are a student Print all of them in one line.
+'''
+name = "Anthony"
+age = 26
+height = 157.6
+is_student = True
+
+print("my name is " + name + " i am " + str(age) + " Years old " + " i am " + str(height) + " cm tall. is " + str(is_student) + " that i am a student of python learning class")
